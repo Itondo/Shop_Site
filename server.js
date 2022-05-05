81ee0bf620aef0261d79109fdbfe3108e9b5deea
Itondo <alexeykolmyk@gmail.com>
2022-05-06 00:38:31 +0300

    Connect BD

--- server.js
+++ server.js
@@ -6,7 +6,7 @@ const fileUpload = require('express-fileupload')
 const cokieParser = require('cookie-parser')
 
 
-const app = express
+const app = express()
 app.use(express.json())
 app.use(cokieParser())
 app.use(cors())
@@ -14,5 +14,24 @@ app.use(fileUpload({
     useTempFiles: true
 }))
 
-// connection to monodb
-const URL
+// connection to mongDB
+const URL = process.env.MONGDB_URL
+mongoose.connect(URL, {
+
+    useNewUrlParser: true,
+    useUnifiedTopology: true
+}, err => {
+    if (err) throw err;
+    console.log('Connected to MongoDB')
+})
+
+
+app.get('/', (req, res) => {
+    res.json("wellcome")
+})
+
+const PORT = process.env.PORT || 5000
+app.listen(PORT, () => {
+    console.log('Server is running on port', PORT)
+
+})
